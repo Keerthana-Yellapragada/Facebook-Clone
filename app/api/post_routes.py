833eb95c7bfbd163a229fb03288ab79cc6156075
Keerthana Yellapragada <keerthanay@gmail.com>
2022-11-22 10:13:24 -0800

    get post details by post id route complete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -4,8 +4,33 @@ from app.models import User, Post, Comment
 
 post_routes = Blueprint('posts', __name__)
 
-@post_routes.route('/')
+# GET ALL POSTS
+@post_routes.route('/', methods=["GET"])
 # @login_required
 def get_posts():
     posts = Post.query.all()
     return {'Posts': [post.to_dict() for post in posts]}
+
+
+
+# GET POST DETAILS BY POST ID
+@post_routes.route('/<int:post_id>/', methods=["GET"])
+# @login_required
+def get_post_details(post_id):
+    post = Post.query.filter(Post.id == post_id).first()
+    # post_user = User.query.filter(User.id == post.user_id).first()
+
+    if post:
+        post_obj = post.to_dict()
+        # post_user_obj = post_user.to_dict()
+        result = {**post.to_dict()}
+        response={**result}
+        return response
+
+    return { "Error": "Post not found" }, 404
+
+
+# CREATE POST
+
+@post_routes.route('/new', methods=["POST"])
+# @login_required
