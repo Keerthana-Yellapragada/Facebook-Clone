809aa8534bebbe3daea7251ec2e110a5046f1366
Keerthana Yellapragada <keerthanay@gmail.com>
2022-11-25 22:26:17 -0800

    added signup validations for email

--- react-app/src/components/SignUpFormModal/SignUpForm.js
+++ react-app/src/components/SignUpFormModal/SignUpForm.js
@@ -1,13 +1,13 @@
 // frontend/src/components/SignupFormPage/SignupForm.js
 import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
-import {useDispatch} from "react-redux";
-import{useHistory} from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import { useHistory } from 'react-router-dom'
 import "./SignUpForm.css"
 
-function SignUpForm(){
+function SignUpForm() {
   const dispatch = useDispatch();
-  const history  = useHistory()
+  const history = useHistory()
   // const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
@@ -18,27 +18,38 @@ function SignUpForm(){
   const [errors, setErrors] = useState([]);
 
 
-  useEffect(()=> {
-    const errors=[];
+  useEffect(() => {
+    const errors = [];
 
     const validUrls = ["com", "io", "org"]
     let urlArray = email.split(".")
-    let urlExtension = urlArray[urlArray.length -1]
+    let urlExtension = urlArray[urlArray.length - 1]
+    const regexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
 
-    if (email && !validUrls.includes(urlExtension) || email.split('@').length < 1) {errors.push("Please enter a valid email")}
-      if (password !== confirmPassword){errors.push('Confirm Password field must be the same as the Password field')}
-      if (username.length < 6) {errors.push("Username must be more than 6 characters")}
-      if (password.length < 6) {errors.push("Password must be more than 6 characters")}
-      if (first_name.length < 2 || last_name.length < 2){errors.push("Names must be more than 2 characters")}
-      setErrors(errors)
 
-  },[first_name, last_name, username, password, confirmPassword, email])
+    const str = "johndoe@example.com";
+
+
+    if (email & !regexExp.test(str)) {errors.push("Please provide valid email") }
+    if (email && !validUrls.includes(urlExtension)) { errors.push("Please provide valid email") }
+    if (email & !email.includes('@')) { errors.push("Please provide a valid email") }
+    if (email & email.split('@').length < 1){ errors.push("Please provide a valid email")}
+    if (password & password !== confirmPassword) { errors.push('Confirm Password field must be the same as the Password field') }
+    if (username & username.length < 6) { errors.push("Username must be more than 6 characters") }
+    if (password & password.length < 6) { errors.push("Password must be more than 6 characters") }
+    if (first_name & first_name.length < 2 || last_name & last_name.length < 2) { errors.push("Names must be more than 2 characters") }
+    if (confirmPassword & password & confirmPassword !== password) {
+      errors.push('Confirm Password field must be the same as the Password field')
+    }
+    setErrors(errors)
+
+  }, [first_name, last_name, username, password, confirmPassword, email])
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
 
-    if (password === confirmPassword){
+    if (password === confirmPassword) {
       setErrors([]);
       return dispatch(sessionActions.signUp({ first_name, last_name, email, username, password })).catch(
         async (res) => {
@@ -58,97 +69,97 @@ function SignUpForm(){
 
 
   return (
-<>
-  <div className="outer-sign-container">
-  <div className="inner-sign-container">
-    <form onSubmit={handleSubmit}>
-      <div className="form-sign-container">
-        <div className="title-sign-container">
-        <h2>Sign Up</h2>
-        <div className="signup-title-extras">It's quick and easy.</div>
+    <>
+      <div className="outer-sign-container">
+        <div className="inner-sign-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-sign-container">
+              <div className="title-sign-container">
+                <h2>Sign Up</h2>
+                <div className="signup-title-extras">It's quick and easy.</div>
+              </div>
+
+              <div className="errors">
+                {errors.map((error, idx) => (
+                  <div key={idx}>{error}</div>
+                ))}
+              </div>
+
+              <div className="inner-form-sign-container">
+
+                <input
+                  className="form-sign-inputs"
+                  type="text"
+                  value={first_name}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                  placeholder="First name"
+                />
+
+
+                <input
+                  className="form-sign-inputs"
+                  type="text"
+                  value={last_name}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                  placeholder="Surname"
+                />
+
+
+                <input
+                  className="form-sign-inputs"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="Email address"
+                />
+
+                <input
+                  className="form-sign-inputs"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  placeholder="Username"
+                />
+
+                <input
+                  className="form-sign-inputs"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  placeholder="New password"
+                />
+
+                <input
+                  className="form-sign-inputs"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  placeholder="Confirm password"
+                />
+
+              </div>
+
+              <div className="disclaimer-content">People who use our service may have uploaded your contact information to WellnessPage. Learn more.
+
+                By clicking Sign Up, you agree to our Terms, Privacy Policy and Cookies Policy. You may receive email notifications from us and can opt out at any time.
+
+              </div>
+              <div className="button-sign-container">
+                <button className="Sign-Up-button" disabled={errors.length > 0} type="submit">Sign Up</button>
+              </div>
+            </div>
+          </form>
         </div>
-
-        <div className="errors">
-        {errors.map((error, idx) => (
-          <div key={idx}>{error}</div>
-        ))}
       </div>
+    </>
 
-      <div className="inner-form-sign-container">
-
-        <input
-        className="form-sign-inputs"
-          type="text"
-          value={first_name}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-          placeholder="First name"
-        />
-
-
-        <input
-        className="form-sign-inputs"
-          type="text"
-          value={last_name}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-          placeholder="Surname"
-        />
-
-
-        <input
-        className="form-sign-inputs"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="Email address"
-        />
-
-        <input
-        className="form-sign-inputs"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          placeholder="Username"
-        />
-
-        <input
-        className="form-sign-inputs"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder="New password"
-        />
-
-        <input
-        className="form-sign-inputs"
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-          placeholder="Confirm password"
-        />
-
-       </div>
-
-        <div className="disclaimer-content">People who use our service may have uploaded your contact information to WellnessPage. Learn more.
-
-        By clicking Sign Up, you agree to our Terms, Privacy Policy and Cookies Policy. You may receive email notifications from us and can opt out at any time.
-
-        </div>
-      <div className="button-sign-container">
-      <button className="Sign-Up-button" disabled={!!errors.length} type="submit">Sign Up</button>
-      </div>
-      </div>
-      </form>
-    </div>
-  </div>
-</>
-
-)
+  )
 
 }
 
