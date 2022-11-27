9ccde43d7b228e0e3790b0d7fa34cd63b14e3f0e
Keerthana Yellapragada <keerthanay@gmail.com>
2022-11-27 10:17:49 -0800

    added delete cascade to user and posts models and it works

--- migrations/versions/054c77c89edb_.py
+++ migrations/versions/054c77c89edb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5f2a9f3363c0
+Revision ID: 054c77c89edb
 Revises: 
-Create Date: 2022-11-25 16:16:22.241064
+Create Date: 2022-11-27 10:14:31.934045
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5f2a9f3363c0'
+revision = '054c77c89edb'
 down_revision = None
 branch_labels = None
 depends_on = None
